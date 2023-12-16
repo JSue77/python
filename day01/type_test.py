6d40ef8a8273612188dcd3d910f79b8d13cc53ae
JSue77 <153965124+JSue77@users.noreply.github.com>
2023-12-16 13:47:30 +0900

    Add files via upload

---
+++ day01/type_test.py
@@ -0,0 +1,29 @@
+#subway= input("서브웨이에서 어떤 메뉴를 고를건가요?")
+#print=(f"{subway} 메뉴는 정말 맛있죠!")
+
+#olive= input("올리브영에서 어떤 제품을 구매하실건가요?")
+#print(f"{olive} 제품은 피부에 참 좋죠!")
+
+
+#a = int(input("첫 번째 수를 입력하세요"))
+#b = int(input("두 번째 수를 입력하세요"))
+#print(f"두 수의 합은 {a+b}")
+#print(f"두 수의 곱은 {a*b}")
+#print(f"두 수의 차는 {a-b}")
+
+
+#int - 정수
+#int(a) # 문자 > 정수화
+#c=int(a) # 문자 > 정수화
+#d=int(b) # 문자 > 정수화
+
+#print(f"두 수의 합은 {a+b}")
+#합 +
+#차 -
+#곱 *
+
+
+a = int(input("첫 번째 수를 입력하세요"))
+b = int(input("두 번째 수를 입력하세요"))
+c = int(input("세 번째 수를 입력하세요"))
+print(f"세 수의 곱은 {a*b*c}")
